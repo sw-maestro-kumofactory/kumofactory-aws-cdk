17b4b11c5963d379cac231fc665f6281c881d796
wook <gus5427@naver.com>
2023-08-14 14:08:41 +0900

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -30,7 +30,7 @@ export class AppService {
     // file write
     fs.writeFileSync('t.json', JSON.stringify(data).toString());
     // execute
-    await exec(['deploy', '--all', '--require-approval', 'never']);
+    await exec(['synth', '--require-approval', 'never']);
     return 'hello';
   }
 }

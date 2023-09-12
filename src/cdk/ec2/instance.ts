c81150c4a46c3394b71bd969da64c33f03d631d7
nookcoder <gus5427@naver.com>
2023-09-12 15:13:59 +0900

    feat : iam, ami 기존값 수정

--- src/cdk/ec2/instance.ts
+++ src/cdk/ec2/instance.ts
@@ -9,6 +9,8 @@ import { getSecurityGroup } from './security-group';
 import { getSubnet } from './subnet';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { EC2Client, DescribeInstancesCommand } from '@aws-sdk/client-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { filter } from 'rxjs';
 
 export const createNewInstance = (scope: Construct, props: Ec2StackOption) => {
   const options = {
@@ -19,9 +21,18 @@ export const createNewInstance = (scope: Construct, props: Ec2StackOption) => {
   const securityGroup = getSecurityGroup(scope, options.securityGroupType);
   const subnet = getSubnet(scope, options.subnetType, options.availabilityZone);
   return new ec2.Instance(scope, props.id, {
+    role: iam.Role.fromRoleArn(
+      scope,
+      'helloworld',
+      'arn:aws:iam::434126037102:role/ecr-full-access',
+    ),
     vpc: vpc,
     instanceType: new ec2.InstanceType(options.instanceType),
-    machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+    // machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+    machineImage: ec2.MachineImage.lookup({
+      name: 'ami-kumo-customer-ubuntu-22.04-x86',
+      owners: ['434126037102'],
+    }),
     vpcSubnets: {
       subnets: [subnet],
     },
@@ -30,6 +41,8 @@ export const createNewInstance = (scope: Construct, props: Ec2StackOption) => {
     instanceName: props.instanceName,
   });
 };
+
+const getIamRole = () => {};
 
 export const getInstanceByInstanceIds = async (instanceIds: [string]) => {
   const input = {
